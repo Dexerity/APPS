b15d02e4cf0c4e0599b60da14c1be9eafed8d122
Martin Ozdinec <ozd.martin@gmail.com>
2024-02-19 11:45:18 +0100

    LE1 Comments

--- Lectures/19.2.2024/LE1/asm-module.asm
+++ Lectures/19.2.2024/LE1/asm-module.asm
@@ -5,30 +5,30 @@
 
     section .text
     global na_velka
-na_velka:
-    mov rcx, 0
+na_velka:       ;for(int i = 0, i != '\0', i++)
+    mov rcx, 0      ;i = 0
 .back:
-    cmp byte [retezec + rcx], 0
+    cmp byte [retezec + rcx], 0     ;i == '\0'
     je .end
 
-    sub byte [retezec + rcx], 'a' - 'A'
+    sub byte [retezec + rcx], 'a' - 'A'    
 
-    inc rcx
+    inc rcx         ;i++
     jmp .back
 .end:
     ret
 
     global suma_pole
-suma_pole:
-    mov eax, 0
-    mov rcx, 0
+suma_pole:          ;for(int i = 0; i < 10; i++)
+    mov eax, 0      ;suma = 0
+    mov rcx, 0      ;i = 0
 .back:
-    cmp rcx, 10
+    cmp rcx, 10     ;i >= 10
     jge .spEnd
 
     add eax, [pole + rcx * 4]
 
-    inc rcx
+    inc rcx         ;i++
     jmp .back
 .spEnd:
     mov [suma], eax
@@ -37,29 +37,29 @@ suma_pole:
     global minmax
 minmax:
     mov eax, [x]
-    mov [max], eax
-    cmp eax, [y]
+    mov [max], eax      ;max = x
+    cmp eax, [y]        ;if x > y
     jge .mmEnd
-    mov eax, [y]
-    mov [max], eax
+    mov eax, [y]        
+    mov [max], eax      ;max = y
 .mmEnd:
     ret
 
     global cosi
 cosi:
-    mov eax, [x]
+    mov eax, [x]                
     mov ecx, [y]
     mov [x], ecx
-    mov [y], eax
+    mov [y], eax                    ;"vymena 2 znaku"
 
     movsx rax, eax
     mov [1], rax
 
-    mov byte [retezec + 4], '!'
+    mov byte [retezec + 4], '!'     ;retezec[4] = '!'
 
-    mov dword [suma] , 0;
+    mov dword [suma] , 0;           ;suma = 0
     mov eax, [x]
-    add [suma], eax
+    add [suma], eax                 ;suma += x
     mov eax, [y]
-    add [suma], eax
+    add [suma], eax                 ;suma += y
     ret
